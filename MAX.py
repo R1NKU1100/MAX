8d0354ad3f8d8da0a1f9e40180055dfc66815bcf
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-01-14 12:37:25 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -1,7 +1,6 @@
 import os,time,platform
 os.system('clear')
 print('[>] Checking Updates......')
-print('[>] Don't Bypass Ok........')
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit=='64bit':
