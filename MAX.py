6c8c1d70a47b0ed49e7d67fc75d58620f0166dee
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-01-17 01:05:12 +0600

    Delete MAX.py

--- MAX.py
+++
@@ -1,9 +0,0 @@
-import os,time,platform
-os.system('clear')
-print('[>] Checking Updates......')
-os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    import MAX
-else:
-    print('\033[1;31m[×] Sorry Device Not Support')
