3dcbdd923b9e00266a178b8847b3dc948035a666
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-01-21 02:07:14 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -1,11 +1,12 @@
 import os,platform,time
 os.system('clear')
-print('[>] Checking Updates...')
+print('[>] Checking Updates.......')
 os.system('git pull')
 from os import path,system
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
+    
     print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
     if path.isfile("PRO.so"):
         pass
