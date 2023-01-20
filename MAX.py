4d18b2b7972948749fb8cbb4a645cc24caabb2ee
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-01-21 02:09:18 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -7,7 +7,7 @@ from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
     
-    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
+    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m');time.sleep(1);
     if path.isfile("PRO.so"):
         pass
     else:
