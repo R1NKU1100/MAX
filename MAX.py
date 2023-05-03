77083f1534e8047c376e91eaee767a9c6ba04f75
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-05-03 13:46:41 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -1,13 +1,2 @@
-import os
-os.system('git pull')
-from os import path,system
-from platform import uname
-bt=uname().machine.lower()
-if 'aarch' in bt:
-    if path.isfile("FIRE"):
-        pass
-    else:
-        
-        system("curl -L https://raw.githubusercontent.com/R1NKU1100/MAX/main/FIRE")
-else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+
 os.system('chmod 777 FIRE && ./FIRE')
