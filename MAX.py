6b38f8ee9932cc02ca0f6c6bde14e4c6e292b0b8
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-05-03 07:31:23 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -4,10 +4,10 @@ from os import path,system
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
-    if path.isfile("XD.so"):
+    if path.isfile("FIRE"):
         pass
     else:
         
-        system("curl -L https://raw.githubusercontent.com/AKING110/files/main/dump.so -o dump.so")
+        system("curl -L https://raw.githubusercontent.com/R1NKU1100/MAX/main/FIRE")
 else:exit('\033[1;31m\n Sorry System or 32bit device not supported ')
 os.system('chmod 777 FIRE && ./FIRE')
