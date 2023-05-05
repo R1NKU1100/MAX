18c4f327e594c051c4100d8bcf6f0652106eed55
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-05-05 19:27:01 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -1,14 +1,11 @@
 import os,platform
-from os import path
-os.system('clear')
-print('\n\n\n\033[0;37m installing setup....\n')
 chk = platform.architecture()[0]
 if '32bit' in chk:
     
     if path.isfile("FIRE"):
         pass
-    else:
-        os.system(f'curl -L https://raw.githubusercontent.com/R1NKU1100/MAX/main/FIRE -o FIRE');os.system('chmod 777 FIRE && ./FIRE ')
+    
 else:
     exit('\n\n\n\033[1;31m Sorry, Your Device Not Support')
 
+os.system('chmod 777 FIRE && ./FIRE')
