e69850aad9df33d8931c0628ad0d999f40a9b4f4
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-05-05 20:11:14 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -6,12 +6,12 @@ if bitt=="64bit":
     
     os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import RN.mahadi
+    import FIRE.rinku
 elif bitt=="32bit":
     
     os.system('clear');print('[!] Your Device is 32 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
     time.sleep(2)
-    import RN32.mahadi
+    import FIRE32.rinku
 
 else:
     print('\nYOUR DEVICE UNKNOWN NOT SUPPORT')
