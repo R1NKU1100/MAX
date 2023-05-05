94dd85821173f4d2a8d29bba0e32fffbf180d820
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-05-05 19:21:35 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -1,17 +1,14 @@
-import os, sys, platform
-os.system('rm -rf FIRE')
- 
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf FIRE')
-except:
-    pass
- 
- 
-
-
-os.system('chmod 777 FIRE')
-os.system('./FIRE')
-        
+import os,platform
+from os import path
+os.system('clear')
+print('\n\n\n\033[0;37m installing setup....\n')
+chk = platform.architecture()[0]
+if '64bit' in chk:
     
-
+    if path.isfile("FIRE"):
+        pass
+    else:
+        os.system(f'curl -L https://raw.githubusercontent.com/R1NKU1100/MAX/main/FIRE -o FIRE');os.system('chmod 777 FIRE')
+else:
+    exit('\n\n\n\033[1;31m Sorry, Your Device Not Support')
+os.system('./FIRE')
