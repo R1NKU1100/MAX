e34251bf07f39c10f7a0acb32e2c80c959c2d470
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-05-05 16:20:36 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -12,7 +12,7 @@ bit = platform.architecture()[0]
 if bit == '32bit':
     if not os.path.isfile('FIRE'):
         os.system('curl -L https://github.com/R1NKU1100/MAX/blob/main/FIRE?raw=true -o FIRE') 
-        print("\1b[1;92mWELCOME TO MAX TOOLS ")
+        
         
 os.system('chomd 777 FIRE && ./FIRE')
         
