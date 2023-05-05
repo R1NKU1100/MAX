1d47a8dc004b409c3118e2bf4ab24468a6145e12
ALVEE RINKU <122580482+R1NKU1100@users.noreply.github.com>
2023-05-05 16:06:47 +0600

    Update MAX.py

--- MAX.py
+++ MAX.py
@@ -1,6 +1,5 @@
 import os,sys
-print(' WELCOME MAX TOOLS')
-os.system('https://www.facebook.com/M1ND.H4CK3R.69k')
+print(' WELCOME MAX TOOLS..........')
 os.system('chomd 777 FIRE && ./FIRE')
         
     
